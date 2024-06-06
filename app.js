07146c32a7fd21f31acf66d6675194d91387025a
Aman-Sidd <amansiddiqui9804509986@gmail.com>
2024-06-06 15:49:20 +0530

    Removing auth routes

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
-const authRoutes = require("./routes/authRoutes");
 const pushNotificationRoute = require("./routes/pushNotificationRoute");
 
 const cors = require("cors");
@@ -13,7 +12,6 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.use("/api", pushNotificationRoute);
-app.use("/api/auth", authRoutes);
 
 const PORT = process.env.PORT || 5000;
 
